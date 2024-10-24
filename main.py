6cc3b7971956605747da9098740d6dd8d194f8c6
Ciaran Sweet <ciaran@developmentseed.org>
2024-10-24 16:22:48 +0100

    feat: playing with basic styling

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-from rich.text import Text
 from textual.app import App, ComposeResult
-from textual.reactive import reactive
-from textual.widgets import Header
+from textual.widgets import Header, Placeholder
 
 
 class GhDash(App):
+    CSS_PATH = "style.tcss"
+
     def compose(self) -> ComposeResult:
         yield Header(show_clock=True)
+        yield Placeholder("Hello", classes="thing")
+        yield Placeholder("Hello", classes="thing")
 
     def on_mount(self) -> None:
         self.title = "ghdash"
         self.sub_title = "What's on your plate today? 🍽️"
 
+
 def entrypoint():
     app = GhDash()
     app.run()
+
 
 if __name__ == "__main__":
     entrypoint()
